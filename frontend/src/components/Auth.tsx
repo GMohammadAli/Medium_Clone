995889628807be4489e47d3f68655904c7963336
GMohammadAli <gulammohammadali@gmail.com>
2025-01-18 21:42:16 +0530

    Removing log statements

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -14,7 +14,6 @@ export const Auth = ({ type }: { type: "signup" | "signin" }) => {
 
   async function sendRequest() {
     try {
-      console.log({BACKEND_URL})
       const response = await axios.post(
         `${BACKEND_URL}/api/v1/user/${type === "signup" ? "signup" : "signin"}`,
         postInputs
